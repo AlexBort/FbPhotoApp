ebc480b97c94f4b725b7cdd20867a299cb18040e
AlexBort <communicationb12345>
2019-06-06 18:31:05 +0300

    made logic for the FacebookGraphApi and for getting albums

---
+++ app/src/main/java/com/example/alex/fbphotoapp/api/RestManager.java
@@ -0,0 +1,51 @@
+package com.example.alex.fbphotoapp.api;
+
+import android.util.Log;
+
+import com.example.alex.fbphotoapp.model.AlbumResponse;
+import com.example.alex.fbphotoapp.mvp.base.BasePresenter;
+import com.example.alex.fbphotoapp.mvp.gallery.AlbumPresenter;
+import com.facebook.AccessToken;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class RestManager {
+
+
+    private FbGraphApi graphApi;
+
+    public RestManager() {
+        this.graphApi = new FbGraphApi();
+    }
+
+    public final void getAlbums(AccessToken token, final AlbumPresenter presenter) {
+        this.graphApi.getApi().getAlbums(token.getUserId(), token.getToken(), "picture,name,created_time").
+                subscribeOn(Schedulers.newThread()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(new Subscriber() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i("onCompleted: ", " completed!");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("error", e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        AlbumResponse response = (AlbumResponse) o;
+                        presenter.responseAlbums(response);
+                    }
+                });
+
+    }
+
+    public final void getPhoto(AccessToken token, BasePresenter presenter) {
+
+
+    }
+
+}
--- app/src/main/java/com/example/alex/fbphotoapp/mvp/gallery/AlbumPresenter.java
+++ app/src/main/java/com/example/alex/fbphotoapp/mvp/gallery/AlbumPresenter.java
@@ -1,9 +1,14 @@
 package com.example.alex.fbphotoapp.mvp.gallery;
 
+import com.example.alex.fbphotoapp.model.AlbumResponse;
 import com.example.alex.fbphotoapp.mvp.base.BasePresenter;
 
 public class AlbumPresenter extends BasePresenter<IAlbumView> {
 
+
+    public void responseAlbums(AlbumResponse response) {
+
+    }
 
     private void requestGallery() {
 //        val albumsList = dao.allAlbums
